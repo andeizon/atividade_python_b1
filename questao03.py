730d3593225ce669080ef064bbfa49e8d545cec4
andeizon <40029041+andeizon@users.noreply.github.com>
2018-06-07 10:27:19 -0300

    Create questao03.py

---
+++ questao03.py
@@ -0,0 +1,30 @@
+r1 = input('telefonou para a vitima?')
+r2 = input('esteve no local do crime?')
+r3 = input('mora perto da vítima?')
+r4 = input('devia para a vítima?')
+r5 = input('já trabalhou com a vítima')
+
+acumulador = 0
+if r1 == "sim":
+    acumulador=acumulador + 1
+
+if r2 == "sim":
+    acumulador=acumulador + 1
+
+if r3 == "sim":
+    acumulador=acumulador +1
+
+if r4 == "sim":
+    acumulador=acumulador +1
+
+if r5 == "sim":
+    acumulador=acumulador +1
+
+if acumulador == 1:
+    print('inocente')
+if acumulador == 2:
+    print('suspeita')
+if acumulador ==3 or acumulador == 4:
+    print('cúplice')
+if acumulador ==5:
+    print('assassino')
